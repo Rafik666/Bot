38196e83c410bc962a1afc33499196a8a8558b1c
Nuggetsik <105106020+Nuggetsik@users.noreply.github.com>
2023-11-12 11:26:06 +0100

    Add files via upload

--- date_base/sqlite_db.py
+++ date_base/sqlite_db.py
@@ -6,10 +6,12 @@ def sql_start():
     cur = base.cursor()
     if base:
         print('Data base connect OK!')
-    base.execute('CREATE TABLE IF NOT EXISTS players(users TEXT, id INTEGER PRIMARY KEY, money INTEGER, win INTEGER)')
+    base.execute('CREATE TABLE IF NOT EXISTS players(users TEXT, id INTEGER PRIMARY KEY, money INTEGER, win INTEGER, lost INTEGER, max_win INTEGER, max_bit INTEGER)')
     base.commit()
 
 async def sql_add_command(state):
     
-    cur.execute('INSERT INTO players VALUES (?,?,?,?)', state)
+    cur.execute('INSERT INTO players VALUES (?,?,?,?,?,?,?)', state)
     base.commit()
+
+
